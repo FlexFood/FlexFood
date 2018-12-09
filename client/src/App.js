363d29be90f897bf7deb481bd3ab1462f66b8c55
cgandiaf <c.gandia.f@gmail.com>
2018-12-09 17:20:49 +0100

    React structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Signup from "./components/auth/Signup";
+import Login from "./components/auth/Login";
+import Message from "./components/Message";
+import AuthService from "./components/auth/AuthService";
+import { Route, Link } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      
+        
       </div>
     );
   }
