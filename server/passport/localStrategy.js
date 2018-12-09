fbf5323106ff147cc00e36fad28500e56dab338c
cgandiaf <c.gandia.f@gmail.com>
2018-12-09 01:39:38 +0100

    Folders public & views removed, not used with react

--- server/passport/localStrategy.js
+++ server/passport/localStrategy.js
@@ -1,27 +1,25 @@
-const passport      = require('passport');
+const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const User          = require('../models/User');
 const bcrypt        = require('bcrypt');
 
-passport.use(new LocalStrategy({
-    usernameField: 'username',
-    passwordField: 'password'
-  }, 
-  (username, password, done) => {
-    User.findOne({ username })
-    .then(foundUser => {
-      if (!foundUser) {
-        done(null, false, { message: 'Incorrect username' });
-        return;
-      }
+passport.use(new LocalStrategy((username, password, next) => {
+  User.findOne({ username }, (err, foundUser) => {
+    if (err) {
+      next(err);
+      return;
+    }
 
-      if (!bcrypt.compareSync(password, foundUser.password)) {
-        done(null, false, { message: 'Incorrect password' });
-        return;
-      }
+    if (!foundUser) {
+      next(null, false, { message: 'Incorrect username' });
+      return;
+    }
 
-      done(null, foundUser);
-    })
-    .catch(err => done(err));
-  }
-));
+    if (!bcrypt.compareSync(password, foundUser.password)) {
+      next(null, false, { message: 'Incorrect password' });
+      return;
+    }
+
+    next(null, foundUser);
+  });
+}));
--- server/passport/serializers.js
+++ server/passport/serializers.js
@@ -1,16 +1,16 @@
 const passport = require('passport');
 const User = require('../models/User');
-
 passport.serializeUser((loggedInUser, cb) => {
   cb(null, loggedInUser._id);
 });
 
 passport.deserializeUser((userIdFromSession, cb) => {
-  User.findById(userIdFromSession)
-  .then(userDocument => {
+  User.findById(userIdFromSession, (err, userDocument) => {
+    if (err) {
+      cb(err);
+      return;
+    }
+
     cb(null, userDocument);
-  })
-  .catch(err => {
-    cb(err);
-  })
+  });
 });
