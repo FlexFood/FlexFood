6fd868ecd2e6a12e46c9d41efeaeeb08502af9b5
Eduardo Gandía Fernández <eduardogandiafernandez@MacBook-Pro-de-Eduardo.local>
2018-12-10 21:54:55 +0100

    query to external API on process

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,21 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import AuthService from "./services/AuthService.js";
-import { Route, Link } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 
-import './App.css';
-
+import "./App.css";
+import Recipes from "./components/recipes";
+import Search from "./components/search";
 import Navbar from "./components/navbar";
 import Login from "./components/login";
-import Signup from "./components/signup"
-
+import Signup from "./components/signup";
 
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      user: null
+      user: null,
+      search: ""
     };
 
     this.authService = new AuthService();
@@ -38,16 +39,41 @@ class App extends Component {
       .then(() => this.setState({ ...this.state, user: null }));
   };
 
+  handleChange = e => {
+    const { value } = e.target;
+    this.setState({ ...this.state, search: value });
+  };
+
   render() {
-   
     return (
       <div className="App">
-      <Navbar user={this.state.user} logout={this.logout} getUser={this.getUser}/>
-        <Route
-          path="/signup"
-          render={() => <Signup getUser={this.getUser} />}
+        <Navbar
+          user={this.state.user}
+          logout={this.logout}
+          getUser={this.getUser}
         />
-        <Route path="/login" render={() => <Login getUser={this.getUser} />} />
+        <Search handleChange={this.handleChange} />
+        <Switch>
+          {/* <Route
+            exact path="/"
+            component={Search}
+            handleChange={this.handleChange}
+            search={this.state.search}
+          /> */}
+          <Route
+            exact path="/recipes"
+            searchPet={this.state.search}
+            component={Recipes}
+          />
+          <Route
+            path="/signup"
+            render={() => <Signup getUser={this.getUser} />}
+          />
+          <Route
+            path="/login"
+            render={() => <Login getUser={this.getUser} />}
+          />
+        </Switch>
       </div>
     );
   }
