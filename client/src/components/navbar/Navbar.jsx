f6783428e8685057f3159a835d73d0cc0c1483e5
Eduardo Gandía Fernández <eduardogandiafernandez@MacBook-Pro-de-Eduardo.local>
2018-12-15 19:22:19 +0100

    Css of main page almost done

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,23 +1,37 @@
-import React, { Component } from 'react'
-import './Navbar.css'
+import React, { Component } from "react";
+import "./Navbar.css";
 import { Link } from "react-router-dom";
 
 export default class Navbar extends Component {
-
   render() {
-    const navBarUserControl = this.props.user ?
-      <li><Link to="/meal">Generador de menús</Link></li>
-      :
-      <li><Link to="/login">Generador de menús</Link></li>
+    const navBarUserControl = this.props.user ? (
+      <li>
+        <Link to="/meal">Menu Generator</Link>
+      </li>
+    ) : (
+      <li>
+        <Link to="/login">Menu Generator</Link>
+      </li>
+    );
     return (
       <div id="navbar">
-        <h1><Link to='/'><img src="" alt="Vacía tu nevera" /></Link></h1>
+        <div id="label-composition">
+          <Link to="/">
+            <h1>RecipesForUse</h1>
+          </Link>
+        </div>
         <ul>
-          <li><Link to="/advancedSearch">Buscador de Recetas</Link></li>
-          <li><Link to="/converter">Conversor</Link></li>
+          <li>
+            <Link to="/advancedSearch">Recipes Finder <span>PRO</span></Link>
+          </li>
+          <li>|</li>
+          <li>
+            <Link to="/converter">Converter</Link>
+          </li>
+          <li>|</li>
           {navBarUserControl}
         </ul>
       </div>
-    )
+    );
   }
 }
--- client/src/components/search/Search.jsx
+++ client/src/components/search/Search.jsx
@@ -1,24 +1,69 @@
 import React, { Component } from "react";
-import { Redirect } from "react-router-dom";
+import { Redirect, Link } from "react-router-dom";
 import "./Search.css";
 
 export default class Search extends Component {
-
   render() {
-
-    if(this.props.redirectToRecipes) {
-      return <Redirect to="/recipes" />
+    if (this.props.redirectToRecipes) {
+      return <Redirect to="/recipes" />;
     }
 
     return (
       <div id="search">
         <form onSubmit={this.props.handleFormSubmit}>
           <input
-            type="text"
+            id="input-search"
+            type="search"
             onChange={e => this.props.handleChange(e)}
           />
-          <input type="submit" value="Buscar" />
+          <input id="input-submit" type="submit" value="search" />
         </form>
+        <div id="box-1">
+          <div id="menu-1">
+            <div className="menu-card">
+              <h2>
+                Recipes Finder <span>PRO</span>
+              </h2>
+              <p>
+                You're late home and you don't know what to eat ... Now
+                everything is much easier. Enter the food you have in your
+                fridge and pantry and we will make a selection of the recipes
+                that best suit you.
+              </p>
+              <Link to="/advancedSearch">Go!</Link>
+            </div>
+          </div>
+          <div id="img-1" />
+        </div>
+        <div id="box-2">
+          <div id="img-2" />
+          <div id="menu-2">
+            <div className="menu-card">
+              <h2>Converter</h2>
+              <p>
+                You have guests and the recipe you are going to prepare is for
+                fewer people than you are going to eat ... Your mother's cake
+                recipe is for 6 people and you are just ... Now it is no longer
+                a problem.
+              </p>
+              <Link to="/converter">Go!</Link>
+            </div>
+          </div>
+        </div>
+        <div id="box-3">
+          <div id="menu-3">
+            <div className="menu-card">
+              <h2>Menu Generator</h2>
+              <p>
+                You do not know what to prepare food day after day ... Now you
+                do not have to weigh more. With a menu generator you can adjust
+                your meals to your tastes and preferences.
+              </p>
+              <Link to="/meal">Go!</Link>
+            </div>
+          </div>
+          <div id="img-3" />
+        </div>
       </div>
     );
   }
