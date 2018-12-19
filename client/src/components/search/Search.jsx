d996773673fd63ccf9a31cc735934269ce4f47a0
cgandiaf <c.gandia.f@gmail.com>
2018-12-19 21:46:17 +0100

    loading in process & control to searc emptyField

--- client/src/components/search/Search.jsx
+++ client/src/components/search/Search.jsx
@@ -1,14 +1,24 @@
 import React, { Component } from "react";
+import { css } from 'react-emotion';
+import { PulseLoader } from 'react-spinners';
 import { Redirect, Link } from "react-router-dom";
 import EdamamService from "../../services/EdamamService";
 import "./Search.css";
 
+const override = css`
+    display: block;
+    margin: 0 auto;
+    border-color: red;
+`;
 
 export default class Search extends Component {
-  constructor(){
+  constructor() {
     super()
     this.state = {
-      search: ""
+      search: "",
+      value: "search",
+      classNameLoading: "on",
+      loading: false
     }
     this.edamamService = new EdamamService();
   }
@@ -21,6 +31,8 @@ export default class Search extends Component {
   handleFormSubmit = e => {
     e.preventDefault();
     const search = this.state.search;
+    if(search === "") return
+    this.loadingChange();
     this.edamamService.getByLabel(search).then(recipes => {
       console.log(recipes)
       this.props.setRecipes(recipes)
@@ -31,9 +43,18 @@ export default class Search extends Component {
       });
     });
   };
-  
+
+  loadingChange = () => {
+    this.setState({
+      ...this.state,
+      classNameLoading: "off",
+      loading: true
+    });
+  }
 
   render() {
+
+
     if (this.state.redirectToRecipes) {
       return <Redirect to="/recipes" />;
     }
@@ -46,7 +67,18 @@ export default class Search extends Component {
             type="search"
             onChange={e => this.handleChange(e)}
           />
-          <input id="input-submit" type="submit" value="search" />
+          <input 
+          id="input-submit" 
+          //className={this.state.classNameLoading}
+          type="submit" 
+          value={this.state.value} />
+          <PulseLoader
+            className={override}
+            sizeUnit={"px"}
+            size={15}
+            color={'#5e5e5e'}
+            loading={this.state.loading}
+          />
         </form>
         <div id="box-1">
           <div id="menu-1">
