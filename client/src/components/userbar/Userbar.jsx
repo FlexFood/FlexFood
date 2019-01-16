7823bb4b96a40353cfe60ee355722ffbb292584d
Eduardo Gandía Fernández <eduardogandiafernandez@MacBook-Pro-de-Eduardo.local>
2019-01-16 10:48:45 +0100

    navbar new

--- client/src/components/userbar/Userbar.jsx
+++ client/src/components/userbar/Userbar.jsx
@@ -1,55 +1,36 @@
-import React, { Component } from 'react'
-import {Link} from "react-router-dom";
-
-import './Userbar.css'
-
-import Modal from "../modal";
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import AuthService from "../../services/AuthService";
 
 export default class Userbar extends Component {
   constructor() {
     super();
-    this.state = {
-      show: false
-    }
+    this.state = {};
+    this.authService = new AuthService();
+    this.fetchUser();
   }
-  showModal = (e) => {
-    this.setState({ show: true, children: e });
-  }
-  //OCULTAR MODAL
-  hideModal = () => {
-    this.setState({ show: false });
-  }
-  
+
+  fetchUser = () => {
+    this.authService
+      .loggedin()
+      .then(user => this.setState({ ...this.state, user }));
+  };
 
   render() {
-
-    const navBarUserControl = this.props.user?
-    <React.Fragment>
-      <li><p>Hi, {this.props.user.username}</p></li>
-      <li>|</li>
-      <li><Link to="/editUser">Edit profile</Link></li>
-      <li><img id="imgProfile" src={this.props.user.pictureUrl} alt="userImg" /></li>
-      <li><Link to="/" onClick={this.props.logout}><img id="off-img" src="./images/power.png" alt=""/> </Link></li>
-    </React.Fragment>
-    :
-    <React.Fragment>
-      <li><Link to="" onClick={()=>this.showModal("Login")} >Login</Link></li>
-      <li><Link to="" onClick={()=>this.showModal("SignUp")} >Signup</Link></li>
-    </React.Fragment>
-    
-    return (
-      <div id="userbar">
-          <ul>
-            {navBarUserControl}
-            <Modal
-              handleClose={this.hideModal}
-              show={this.state.show}
-              children= {this.state.children}
-              close={this.hideModal}
-              setUser={this.props.setUser} />
-          </ul>
-          
+    const userbarLinksToggle = this.state.user ? (
+      <div>
+        <p>
+          Hi, {this.state.user.username} | <Link to="/editUser">Edit</Link>
+        </p>
+        <img src={this.state.user.pictureUrl} alt={this.state.user.username} />
+        <img onClick={this.logOut} src="../../images/power.png" alt="logout-button" />
       </div>
-    )
+    ) : (
+      <div>
+        <button onClick={this.loginToggle}>Login</button>
+        <button onClick={this.signInToggle}>Signin</button>
+      </div>
+    );
+    return <div>{userbarLinksToggle}</div>;
   }
 }
